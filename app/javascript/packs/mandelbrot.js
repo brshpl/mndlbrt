4a904bea5f7827a129b446e417459ddaff85a800
Igor <igor_barishpol@mail.ru>
2019-12-20 17:09:23 +0300

    pre-alpha

---
+++ app/javascript/packs/mandelbrot.js
@@ -0,0 +1,166 @@
+function map(value, domain, range, isDecel){
+    if (isDecel){
+        return range[0] + (range[1] - range[0]) * interCubeDecel((value - domain[0]) / (domain[1] - domain[0]));
+    } else {
+        let final = range[0] + (range[1] - range[0]) * (value - domain[0]) / (domain[1] - domain[0]);
+        final = final < range[0] ? range[0] : final > range[1] ? range[1] : final;
+        return final;
+    }
+}
+
+function interCubeDecel(x){
+    v0 = -2;
+    v1 = 0;
+    v2 = 1;
+    v3 = 0;
+
+    P = (v3 - v2) - (v0 - v1);
+    Q = (v0 - v1) - P;
+    R = v2 - v0;
+    S = v1;
+
+    return P*x*x*x + Q*x*x + R*x + S;
+}
+
+function mapColor(value,domain,rgb1,rgb2){
+    let r = Math.floor(map(value, domain, [rgb1[0], rgb2[0]]));
+    let g = Math.floor(map(value, domain, [rgb1[1], rgb2[1]]));
+    let b = Math.floor(map(value, domain, [rgb1[2], rgb2[2]]));
+    let a = Math.floor(map(value, domain, [rgb1[3], rgb2[3]]));
+    r = r < 0 ? 0 : r > 255 ? 255 : r;
+    g = g < 0 ? 0 : g > 255 ? 255 : g;
+    b = b < 0 ? 0 : b > 255 ? 255 : b;
+    a = a < 0 ? 0 : a > 255 ? 255 : a;
+    return [r,g,b,a];
+}
+
+function Mandelbrot(){
+    this.ratio          = .5;                                    //window.devicePixelRatio * 2;
+    this.canvas         = document.getElementById('canvas');
+    this.canvas.width   = window.innerWidth  * this.ratio;
+    this.canvas.height  = window.innerHeight * this.ratio;
+    this.ctx            = this.canvas.getContext('2d');
+    this.chunkSize      = Math.floor(this.canvas.width / 8);
+    this.vcanvas        = document.createElement('canvas');
+    this.vcanvas.width  = this.canvas.width;
+    this.vcanvas.height = this.canvas.height;
+    this.vctx           = this.vcanvas.getContext('2d');
+    this.palette        = 3;
+
+    //this.zoom = 20000000;                           //Satellite valley (VERY SLOW)
+    //this.center = {x: -.74364386, y: .1318259};
+    //this.maxIterations = 3000;
+
+    //this.zoom = 3000000;                            //Satellite valley 2 (VERY SLOW)
+    //this.center = {x: -.7667868, y: .10826};
+    //this.maxIterations = 3000;
+
+    //this.zoom = 200000;                             //Satellite
+    //this.center = {x: -.743643, y: .131826};
+    //this.maxIterations = 1600;
+
+    //this.zoom = 1000;                               //Star
+    //this.center = {x: -.81045, y: .20175};
+    //this.maxIterations = 180;
+
+    //this.zoom = 600;                                //Wally
+    //this.center = {x: -.7674, y: .108};
+    //this.maxIterations = 360;
+
+    this.zoom = 0.8;                                  //Initial
+    this.center = {x: -1, y: 0};
+    this.maxIterations = 50;
+
+    //this.zoom = 150                                 //Burning ship (needs change in worker code)
+    //this.center = {x: 1.624, y: 0.01};
+    //this.maxIterations = 80;
+
+    //this.maxIterations = function() {return Math.floor(map(this.zoom,[1,2000000],[50,3000]))};
+    this.range = function() {return 1 / this.zoom};
+    this.scale = function() {return this.canvas.height / (this.range() * 2)};
+    this.bias  = function() {return {x:   this.canvas.width  / 2 - this.center.x * this.scale(),
+                                     y: - this.canvas.height / 2 - this.center.y * this.scale()}};
+
+    this.renderUnit = function(ctx,message){
+        this.worker = new Worker('packs/js/worker-9df8c22d0e0a704f758f.js');
+        this.worker.onmessage = function(event){
+            ctx.putImageData(event.data.map,
+                             event.data.origin.x,
+                             event.data.origin.y);
+            this.worker.terminate();
+        }.bind(this);
+        if (message){
+            this.worker.postMessage(message);
+        }
+    };
+
+
+    this.setColors = function(palette){
+        this.palette = palette;
+        this.refine(1);
+    };
+    this.refine = function(ratio,iterations){
+        let currentRatio = this.ratio;
+        if (ratio === 1){
+            this.ratio = 0.5;
+        } else if (ratio === 2){
+            this.ratio = window.devicePixelRatio;
+        } else if (ratio === 3){
+            this.ratio = window.devicePixelRatio * 2;
+        }
+
+        if (iterations > 10 && iterations <= 3000){
+            this.maxIterations = iterations;
+        }
+
+        if (currentRatio !== this.ratio){
+            this.vcanvas.width  = this.canvas.width;
+            this.vcanvas.height = this.canvas.height;
+            this.vctx.drawImage(this.canvas, 0, 0);
+            this.canvas.width  = window.innerWidth  * this.ratio;
+            this.canvas.height = window.innerHeight * this.ratio;
+            this.ctx.drawImage(this.vcanvas, 0, 0, this.canvas.width, this.canvas.height);
+        }
+        this.chunkSize = Math.floor(this.canvas.width / 8);
+        this.render();
+    };
+
+    this.canvas.addEventListener('click', function(e){
+        if (e.clientX < 0.9 * this.canvas.width / this.ratio){
+            this.center = {x: ( e.clientX * this.ratio - this.bias().x) / this.scale(),
+                           y: (-e.clientY * this.ratio - this.bias().y) / this.scale()};
+            this.zoom *= 2;
+            console.log(this.maxIterations, this.zoom);
+        } else {
+            this.zoom = this.zoom / 2;
+            console.log(this.maxIterations, this.zoom);
+        }
+        this.refine(1);
+    }.bind(this));
+
+}
+
+Mandelbrot.prototype.showMessage = function(progress){
+    //this.ctx.clearRect(0,0,canvas.width,canvas.height);
+    this.ctx.font = 20*this.ratio +'px Arial';
+    this.ctx.fillText('Rendering ' + this.canvas.width + ' x ' + this.canvas.height + ' image ... ' + progress,
+                      20 * this.ratio,
+                      40 * this.ratio);
+};
+
+Mandelbrot.prototype.render = function(){
+    for (let y = 0; y < this.canvas.height; y += this.chunkSize){
+        for (let x = 0; x < this.canvas.width; x += this.chunkSize){
+            new this.renderUnit(this.ctx, {map:           this.ctx.createImageData(this.chunkSize, this.chunkSize),
+                                           bias:          this.bias(),
+                                           scale:         this.scale(),
+                                           maxIterations: this.maxIterations,
+                                           origin:        {x:x, y:y},
+                                           palette:       this.palette});
+        }
+    }
+    this.showMessage('');
+};
+
+var mandelbrot = new Mandelbrot();
+mandelbrot.render();
---
+++ public/worker.js
@@ -0,0 +1,194 @@
+onmessage = function(event){
+
+    render(event.data.map);
+
+    function map(value,domain,range,isDecel){
+        if (isDecel){
+            return range[0] + (range[1] - range[0]) * interCubeDecel((value - domain[0]) / (domain[1] - domain[0]));
+        } else {
+            return range[0] + (range[1] - range[0]) * (value - domain[0]) / (domain[1] - domain[0]);
+        }
+    }
+    function interCubeDecel(x){
+        v0 = -2;
+        v1 = 0;
+        v2 = 1;
+        v3 = 0;
+
+        P = (v3 - v2) - (v0 - v1);
+        Q = (v0 - v1) - P;
+        R = v2 - v0;
+        S = v1;
+
+        return P * x*x*x + Q * x*x + R * x + S;
+    }
+    function mapColor(fraction, stops, colors){
+        fraction = fraction > 1 ? 1 : fraction < 0 ? 0 : fraction;
+        let marker = undefined;
+        for (let i = 1; i < stops.length; i++){
+            if (fraction < stops[i]){
+                marker = i - 1;
+                break;
+            }
+        }
+        try{
+            if (marker != undefined){
+                let from = stops[marker];
+                let to = stops[marker + 1];
+                let colorFrom = colors[marker];
+                let colorTo = colors[marker + 1];
+                let r = Math.floor(map(fraction,[from,to],[colorFrom[0],colorTo[0]]));
+                let g = Math.floor(map(fraction,[from,to],[colorFrom[1],colorTo[1]]));
+                let b = Math.floor(map(fraction,[from,to],[colorFrom[2],colorTo[2]]));
+                let a = Math.floor(map(fraction,[from,to],[colorFrom[3],colorTo[3]]));
+
+                r = r < 0 ? 0 : r > 255 ? 255 : r;
+                g = g < 0 ? 0 : g > 255 ? 255 : g;
+                b = b < 0 ? 0 : b > 255 ? 255 : b;
+                a = a < 0 ? 0 : a > 255 ? 255 : a;
+                return [r,g,b,a];
+            }
+        } catch (err){
+            console.log(colors, marker);
+        }
+    }
+
+    function mapColorCycle(fraction, stops, colors, cycleFraction){
+        let output;
+        let scaledStops = [];
+        let cycledColors = colors;
+        cycledColors.unshift(cycledColors.pop());
+        if (fraction > 1 - cycleFraction) {
+            output = mapColorCycle((fraction + cycleFraction - 1) / cycleFraction,stops,cycledColors,cycleFraction * cycleFraction);
+        } else {
+            output = mapColor(fraction / (1 - cycleFraction),stops,colors);
+        }
+        return output;
+    }
+
+    function xyToZ(xy, bias, scale){
+        let x1 = ( xy.x - bias.x) / scale;
+        let y1 = (-xy.y - bias.y) / scale;
+        return {x: x1, y: y1};
+    }
+    function isIn(xy, maxIterations){
+        let z = {x: 0, y: 0};
+        let probe = xyToZ(xy, event.data.bias, event.data.scale);
+        let final = false;
+        let value = 0;
+        let iterations = 0;
+        for (let i = 0; i < maxIterations; i++){
+
+            let zSquare = {x: z.x * z.x - z.y * z.y,
+                           y: 2   * z.x       * z.y};
+            z.x = probe.x + zSquare.x;
+            z.y = probe.y + zSquare.y;
+            value = (z.x * z.x + z.y * z.y);
+
+            if (value <= 16) {
+                iterations ++;
+                final = true;
+            } else {
+                final = false;
+                i = maxIterations;
+            }
+        }
+        return {switch:     final,
+                value:      value,
+                iterations: iterations,
+                normalized: iterations - (Math.log(Math.log(value))) / Math.log(2.0)};
+    }
+
+    function setPixelColor(map, x, y, rgba, scan){
+        map.data[scan.offset++] = rgba[0] || 0;
+        map.data[scan.offset++] = rgba[1] || 0;
+        map.data[scan.offset++] = rgba[2] || 0;
+        map.data[scan.offset++] = rgba[3] || 255;
+    }
+
+    function render(pixels){
+        let scan = {offset: 0};
+        for (let y = 0; y < pixels.height; y++){
+            for (let x = 0; x < pixels.width; x++){
+                let result = isIn({x: x + event.data.origin.x,
+                                   y: y + event.data.origin.y},
+                                  event.data.maxIterations);
+                let fraction = result.normalized / event.data.maxIterations;
+                if (result.switch){
+                    if (event.data.palette === 5){
+                        setPixelColor(pixels, x, y, [0,0,0,255], scan);
+                    } else {
+                        scan.offset += 4;
+                    }
+                } else {
+                    switch(event.data.palette){
+                        case 1:
+                            setPixelColor(pixels, x, y,
+                                          [0,0,0,255],
+                                          scan);
+                            break; // Monochrome
+                        case 2:
+                            setPixelColor(pixels, x, y,
+                                          mapColorCycle(fraction, [0,1], [[0,0,0,255],[255,255,255,255]], .9),
+                                          scan);
+                            break; //BW Cycle
+                        case 3:
+                            setPixelColor(pixels, x, y,
+                                          mapColorCycle(fraction, [0,.2,.4,.6,.8,1], [[89,14,34,255],[12,3,68,255],[52,137,218,255],[255,255,255,255],[255,210,47,255],[174,67,13,255]], .8),
+                                          scan);
+                            break; // Sunshine
+                        case 4:
+                            setPixelColor(pixels, x, y,
+                                          mapColorCycle(fraction, [0,.15,.3,.48,.62,.7,.85,1], [[58,15,110,255],[140,40,129,255],[221,73,104,255],[253,158,108,255],[251,251,190,255],[253,158,108,255],[221,73,104,255],[140,40,129,255]], .8),
+                                          scan);
+                            break; // Inferno cycle
+                        case 5:
+                            setPixelColor(pixels, x, y,
+                                          mapColorCycle(fraction, [0,.1,.4,.6,1], [[255,255,255,255],[252,237,177,255],[154,75,89,255],[32,42,66,255],[0,0,0,255]], .6),
+                                          scan);
+                            break; // Gothic cycle
+                        case 6:
+                            setPixelColor(pixels, x, y,
+                                          mapColor(fraction, [0,.1,.4,.6,1], [[255,255,255,255],[252,237,177,255],[154,75,89,255],[32,42,66,255],[0,0,0,255]]),
+                                          scan);
+                            break; // Gothic
+                        case 7:
+                            setPixelColor(pixels, x, y,
+                                          mapColor(fraction, [0,.1,.3,.5,1], [[44,44,46,255],[103,48,43,255],[199,75,51,255],[249,157,80,255],[247,246,163,255]]),
+                                          scan);
+                            break; // Flame
+                        case 8:
+                            setPixelColor(pixels, x, y,
+                                          mapColor(fraction, [0,.3,.6,.8,1], [[0,0,0,255],[24,40,85,255],[121,61,143,255],[255,228,133,255],[255,255,255,255]]),
+                                          scan);
+                            break; // Light in the darkness
+                        case 9:
+                            setPixelColor(pixels, x, y,
+                                          mapColor(fraction, [0,.2,.4,.6,.8,1], [[0,0,0,255],[58,15,110,255],[140,40,129,255],[221,73,104,255],[253,158,108,255],[251,251,190,255]]),
+                                          scan);
+                            break; // Inferno
+                        case 10:
+                            setPixelColor(pixels, x, y,
+                                          mapColor(fraction, [0,.2,.3,.4,.5,1], [[0,0,0,255],[128,0,255,255],[0,0,255,255],[0,255,0,255],[255,0,0,255],[251,251,190,255]]),
+                                          scan);
+                            break; // Acid
+                        case 11:
+                            setPixelColor(pixels, x, y,
+                                          mapColor(fraction, [0,1], [[0,0,0,255],[255,255,255,255]]),
+                                          scan);
+                            break; //BW
+                        default:
+                            setPixelColor(pixels, x, y,
+                                          [0,0,0,255],
+                                          scan);
+                            break;
+                    }
+                }
+            }
+        }
+        postMessage({map:    pixels,
+                     type:   2,
+                     origin: {x: event.data.origin.x,
+                              y: event.data.origin.y}});
+    }
+};
